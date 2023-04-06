85f4ffae8168bea6ba1cffdeb93bf6fa9275af86
amina <aminagourma88@gmail.com>
2023-04-06 12:14:07 +0000

    amina modified

--- code.py
+++ code.py
@@ -1,2 +1,6 @@
 language = "python"                                
 reversed_language = language[::-1]                                                                 print(reversed_language) # nohtyp
+ 
+#amina 
+#mm 
+#123
