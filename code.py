9d02e5121942f50451892440cda3a18b1449e48c
saloual01 <118121366+saloual01@users.noreply.github.com>
2023-04-06 12:57:21 +0000

    Update code.py

--- code.py
+++ code.py
@@ -1,11 +1,3 @@
 language = "python"                                
 reversed_language = language[::-1]                                                                 print(reversed_language) # nohtyp
  
-#amina 
-#mm 
-#123
-
-#ana modifiet hnaya
-
-
-#modification_amina_2
