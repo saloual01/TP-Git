eb6b7105feca2fed008efe50badb115356afd055
saloual01 <etu_loussaoui.saloua@emsi-edu.ma>
2023-04-06 13:18:47 +0000

    New Modifications

--- code.py
+++ code.py
@@ -1,3 +1,19 @@
-language = "python"                                
-reversed_language = language[::-1]                                                                 print(reversed_language) # nohtyp
- 
+z = complex(5, 7)
+print("Output:", z)
+# Output: (5+7j) 
+
+z = complex(3)
+print("Output:", z)
+# Output: (3+0j)
+
+z = complex()
+print("Output:", z)
+# Output: 0j
+
+z = complex('1+1j')
+print("Output:", z)
+# Output: 1+1j
+
+z = complex(1, -4)
+print("Output:", z)
+# Output: 1-4j
